d84d2b81cb265ebfefbde5e497e3942dffaf26e2
Nana Kwesi Safo <99858021+safoisgod@users.noreply.github.com>
2025-03-04 17:36:20 +0000

    adkay

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menu = document.querySelector('.menu-space');
+    const hamburgerIcon = document.querySelector('.hamburger-icon');
+    menu.classList.toggle('active');
+    hamburgerIcon.classList.toggle('active');
+}
+
+
